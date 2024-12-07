35fb848d651096bb40070aa2e1b71f916ec797df
Aviweiam <nika.pomazova@gmail.com>
2024-12-07 17:53:53 -0500

    jdshfkhksdfhlkjc

--- Routine.java
+++ Routine.java
@@ -1,3 +1,30 @@
 public class Routine {
+    private String name;
+    private String stime,etime;
+
+
+    public Routine(String name, String s,String e) {
+        this.name = name;
+        this.etime = e;
+        this.stime = s;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStime() {
+        return stime;
+    }
+
+    public String getEtime() {
+        return etime;
+    }
+
+    
+    @Override
+    public String toString() {
+        return name+", from "+stime+" to "+etime;
+    }
 
 }
--- Schedual.java
+++ Schedual.java
@@ -1,3 +1,36 @@
+import java.util.ArrayList;
+
 public class Schedual {
 
+
+    ArrayList<Routine> routinearray;
+    ArrayList<Task> taskarray;
+    public Schedual(ArrayList<Task> a, ArrayList<Routine> b){
+
+        this.routinearray = b;
+        this.taskarray = a;
+       String[] schedual = new String[24]; 
+       double[] hours = new double[24]; 
+        for (int m=0; m<24; m++){
+            hours[m] = -1;
+        }
+
+       for(int i =0; i<routinearray.size();i++){
+        int stime = Integer.parseInt(routinearray.get(i).getStime());
+        int etime = Integer.parseInt(routinearray.get(i).getEtime());
+        for(int n = stime; n <= etime;n++){
+            schedual[n]=routinearray.get(i).getName();
+            hours[n]= 0;
+        }
+
+       }
+
+       for (int y=0; y<24; y++){
+            if(hours[y]<0){
+                schedual[y]=taskarray.get(y).getName();
+                taskarray.get(y).usedHours(1);
+            }
+        }
+
+    }
 }
